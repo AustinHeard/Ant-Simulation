85ff12f747ac0a787b21b13a09fefb89c738a19d
Austin Heard <austinheard55@gmail.com>
2023-12-10 03:07:16 -0500

    Docs about Utility functions

--- Utility.js
+++ Utility.js
@@ -10,12 +10,18 @@ class Utility {
 		text("FPS: "+round(fps), width - 100, 50);
 	}
 
+	/**
+	 * Show number of Food in the top right corner
+	 */
 	showNumberFood() {
 		fill(256);
 		textSize(20);
 		text("Number of Food: "+FoodList.length, width - 200, 100)
 	}
 
+	/**
+	 * Show number of Ants in the top right corner
+	 */
 	showNumberAnts() {
 		fill(256);
 		textSize(20);
